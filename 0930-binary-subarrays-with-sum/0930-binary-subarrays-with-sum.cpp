b53fbb3be87caf2ac68925d77bed481e4f04eb12
Sunny7135 <150102341+Sunny7135@users.noreply.github.com>
2026-02-23 12:55:49 +0530

    Time: 0 ms (100%), Space: 32.7 MB (40.43%) - LeetHub

--- 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.cpp
+++ 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.cpp
@@ -1,23 +1,22 @@
 class Solution {
 public:
-    int almost(vector<int>& nums, int goal){
-          if(goal<0)return 0;
-        int l =0, r=0, count =0;
-        int n= nums.size();
-        // vector<int> hash = (2, 0);
-        int sum =0;
-        while(r < n){
-            sum+=nums[r];
-            while(sum>goal){
-                sum -= nums[l];
-                l++;
+    int almost(vector<int>&nums, int goal){
+            if(goal<0)return 0;
+        int r=0, l=0, n = nums.size();
+        int i=0;
+        int count =0;
+        while(r<n){
+            l+=nums[r];
+            while(l>goal){
+                l -= nums[i];
+                i++;
             }
-           count += (r-l+1);
-           r++;
+            count += (r-i+1);
+            r++;
         }
         return count;
     }
     int numSubarraysWithSum(vector<int>& nums, int goal) {
-      return (almost(nums, goal) - almost(nums, goal-1));
-    }
+        return(almost(nums, goal)- almost(nums, goal-1));
+    }    
 };
