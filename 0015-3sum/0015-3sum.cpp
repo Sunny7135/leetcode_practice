e81475dfd19a00998fc38d705b98acfe465f44d1
Sunny7135 <150102341+Sunny7135@users.noreply.github.com>
2026-02-25 03:42:29 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -5,7 +5,7 @@ public:
         sort(nums.begin(), nums.end());
         vector<vector<int>>ans;
         for(int i=0; i<n-2; i++){
-            if(i>0 && nums[i] == nums[i-1])continue;
+            // if(i>0 && nums[i] == nums[i-1])continue;
             int l= i+1;
             int r= n-1;
             while(l<r){
