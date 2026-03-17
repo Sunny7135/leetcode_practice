9c2d20e0c84ca847d1cf3a1d94505470211dd87f
Sunny7135 <150102341+Sunny7135@users.noreply.github.com>
2026-03-17 22:01:16 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,32 +1,18 @@
-int Qs(vector<int> &nums, int low, int high){
-    int pivot = nums[low];
-    int i= low;
-    int j= high;
-    while(i<j){
-        while(nums[i] <= pivot && i <= high-1)i++;
-        while(nums[j] >  pivot && j >= low+1)j--;
-        if(i<j)swap(nums[i], nums[j]);
-    }
-    swap(nums[low], nums[j]);
-    return j;
-}
-
-void qs(vector<int> &nums, int low, int high){
-    if(low< high){
-    int pindex = Qs(nums, low, high);
-        qs(nums, low, pindex-1);
-        qs(nums, pindex+1, high);
-    }
-}
-
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        qs(nums, 0, nums.size()-1);
-        // return nums;
-
-        for(int x : nums){
-            cout << x <<" ";
+        int j =0, i=0, k=nums.size()-1;
+        while(j<=k){
+            if(nums[j]== 0){
+                swap(nums[j], nums[i]);
+                j++;
+                i++;
+            }else if(nums[j] == 1){
+                j++;
+            }else{
+                swap(nums[j], nums[k]);
+                k--;
+            }
         }
     }
 };
