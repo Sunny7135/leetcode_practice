5d1ff8afdccf6cbfd358baddcb4bf25d993fb591
Sunny7135 <150102341+Sunny7135@users.noreply.github.com>
2026-04-23 22:06:48 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,6 +1,25 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        next_permutation(nums.begin(), nums.end());
+        int in =-1;
+        int j = nums.size() ;
+        for(int i =j-2; i>=0; i--){
+            if(nums[i] < nums[i+1]){
+                in = i;
+                break;
+            }
+        }
+        if(in == -1){
+            reverse(nums.begin(), nums.end());
+        }else{
+            for(int i = j-1; i>=0; i--){
+            if(nums[i] > nums[in]){
+                swap(nums[i], nums[in]);
+                break;
+            }
+        }
+        reverse(nums.begin() + in + 1, nums.end());
+        }
+        
     }
 };
