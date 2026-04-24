3804962a2cb974e21708b75c3868e2806e5aac17
Sunny7135 <150102341+Sunny7135@users.noreply.github.com>
2026-04-24 18:44:57 +0530

    Time: 57 ms (22.24%), Space: 28.9 MB (99.54%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -4,15 +4,16 @@ public:
         sort(nums.begin(), nums.end());
         int n = nums.size();
         vector<vector<int>> ans;
-        for(int i=0; i<nums.size()-2; i++){
+        for(int i=0; i<n-2; i++){
+            if(i>0 && nums[i] == nums[i-1])continue;
             int j = i+1;
             int k = n-1;
             while (j < k){
                 int sum = (nums[i] + nums[k] + nums[j]);
                 if(sum == 0){
                     ans.push_back({nums[i], nums[j], nums[k]});
-                    while(j<k && nums[i] == nums[i+1])j++;
-                    while(j<k && nums[k] == nums[k-1])k++;
+                    while(j<k && nums[j] == nums[j+1])j++;
+                    while(j<k && nums[k] == nums[k-1])k--;
                     j++;
                     k--; 
                 }
