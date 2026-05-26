55831a66ca2dca52a8507a506fb171f71d594aaa
sunny kardam <150102341+Sunny7135@users.noreply.github.com>
2026-05-26 15:38:18 +0530

    Time: 59 ms (55.96%), Space: 21.2 MB (42.81%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.cpp
+++ 0127-word-ladder/0127-word-ladder.cpp
@@ -7,14 +7,13 @@ public:
         int step =1;
         while(!q.empty()){
             string word = q.front().first;
-            step = q.front().second;
+            int step =q.front().second;
             q.pop();
-            
             if(word == endWord)return step;
 
             for(int i=0; i<word.size(); i++){
                 char original = word[i];
-                for(char ch = 'a'; ch<='z'; ch++){
+                for(char ch = 'a'; ch<= 'z'; ch++){
                     word[i] = ch;
                     if(st.find(word) != st.end()){
                         q.push({word, step+1});
