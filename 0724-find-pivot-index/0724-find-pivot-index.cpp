58b5e441c59ce9e4db6362be59a579f5a270b5c6
sunny kardam <150102341+Sunny7135@users.noreply.github.com>
2026-06-25 15:20:39 +0530

    Time: 0 ms (100%), Space: 35.8 MB (30.32%) - LeetHub

---
+++ 0724-find-pivot-index/0724-find-pivot-index.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+        int sum=0;
+        for(int i : nums){
+            sum += i;
+        }
+        int l =0;
+        for(int i=0; i<nums.size(); i++){
+            int r = sum - l - nums[i];
+            
+            if(l == r)return i;
+            l += nums[i];
+        }
+        return -1;
+    }
+};
