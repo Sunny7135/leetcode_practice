8a361bc1eab9be730b49bf02f16af2aa2c570241
sunny kardam <150102341+Sunny7135@users.noreply.github.com>
2026-07-22 17:15:06 +0530

    Time: 2 ms (57.75%), Space: 7.9 MB (73.42%) - LeetHub

--- 0278-first-bad-version/0278-first-bad-version.cpp
+++ 0278-first-bad-version/0278-first-bad-version.cpp
@@ -4,15 +4,15 @@
 class Solution {
 public:
     int firstBadVersion(int n) {
-        long long i=1, j= n;
+        long long i =1, j =n;
         while(i<=j){
             long long mid = (i+j)/2;
             if(isBadVersion(mid)){
-                j = mid-1;
+                j= mid-1;
             }else{
                 i = mid+1;
             }
-        }      
+        }
         return i;
     }
 };
