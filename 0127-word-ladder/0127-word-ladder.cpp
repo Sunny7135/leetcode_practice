6faa33fec8d4c09109fd2bad1ba36f98c1cc26f3
sunny kardam <150102341+Sunny7135@users.noreply.github.com>
2026-08-17 17:42:29 +0530

    Time: 57 ms (56.95%), Space: 21.3 MB (40.94%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.cpp
+++ 0127-word-ladder/0127-word-ladder.cpp
@@ -4,19 +4,19 @@ public:
         unordered_set<string>st(wordList.begin(), wordList.end());
         queue<pair<string, int>>q;
         q.push({beginWord, 1});
-        int step =1;
+        int step = 1;
         while(!q.empty()){
             string word = q.front().first;
-            int step =q.front().second;
+            step = q.front().second;
             q.pop();
-            if(word == endWord)return step;
 
+            if(word == endWord)return step;
             for(int i=0; i<word.size(); i++){
                 char original = word[i];
-                for(char ch = 'a'; ch<= 'z'; ch++){
-                    word[i] = ch;
+                for(char c = 'a'; c<='z'; c++){
+                    word[i] = c;
                     if(st.find(word) != st.end()){
-                        q.push({word, step+1});
+                        q.push({word, step +1});
                         st.erase(word);
                     }
                 }
