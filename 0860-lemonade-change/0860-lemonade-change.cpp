7332766b76e7a86228a3f3a6a249b3209dbe4562
sunny kardam <150102341+Sunny7135@users.noreply.github.com>
2026-08-25 18:22:07 +0530

    Time: 0 ms (100%), Space: 87.3 MB (44.97%) - LeetHub

--- 0860-lemonade-change/0860-lemonade-change.cpp
+++ 0860-lemonade-change/0860-lemonade-change.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
     bool lemonadeChange(vector<int>& bills) {
-        int five =0, ten =0;
-        for(int x : bills){
-            if(x == 5){
+        int five=0, ten=0;
+        for(int i : bills){
+            if(i == 5){
                 five++;
-            }else if(x == 10){
+            }else if(i == 10){
                 if(five){
                     five--;
                     ten++;
@@ -16,8 +16,8 @@ public:
                 if(five && ten){
                     five--;
                     ten--;
-                }else if(five >= 3){
-                    five -=3;
+                }else if(five >=3){
+                    five -= 3;
                 }else{
                     return false;
                 }
