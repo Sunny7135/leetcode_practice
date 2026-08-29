9e45ac5a75ab95eef95850e95d00f44059a32d33
sunny kardam <150102341+Sunny7135@users.noreply.github.com>
2026-08-29 13:58:09 +0530

    Time: 47 ms (63.28%), Space: 29.2 MB (46.09%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -6,22 +6,22 @@ public:
         int n = nums.size();
         for(int i=0; i<n-2; i++){
             if(i>0 && nums[i] == nums[i-1])continue;
-            int j = i+1;
-            int k = n-1;
+            int j=i+1;
+            int k=n-1;
             while(j<k){
                 int sum = nums[i] + nums[j] + nums[k];
-                if(sum == 0){
-                ans.push_back({nums[i], nums[j], nums[k]});
-                while(j<k && nums[j] == nums[j+1])j++;
-                while(j<k && nums[k] == nums[k-1])k--;
-                j++;
-                k--; 
+                if(sum ==0){
+                    ans.push_back({nums[i], nums[j], nums[k]});
+                    while(j<k && nums[j] == nums[j+1])j++;
+                    while(j<k && nums[k] == nums[k-1])k--;
+                    j++;
+                    k--;
                 }else if(sum<0){
                     j++;
                 }else{
                     k--;
                 }
-            }    
+            }
         }
         return ans;
     }
