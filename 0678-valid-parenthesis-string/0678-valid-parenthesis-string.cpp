ca8fac0bde4f45b49471aaac18ccdc58948c962b
sunny kardam <150102341+Sunny7135@users.noreply.github.com>
2026-09-10 15:57:43 +0530

    Time: 0 ms (100%), Space: 8.2 MB (27.66%) - LeetHub

---
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool checkValidString(string s) {
+        int l =0, r=0;
+
+        for(char c : s){
+            if(c == '('){
+                l++;
+                r++;
+            }else if(c == ')'){
+                l--;
+                r--;
+            }else{
+                l--;
+                r++;
+            }
+
+            if(r<0)return false;
+            l = max(l,0);
+        }
+        return l==0;
+        
+    }
+};
