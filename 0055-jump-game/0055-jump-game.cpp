98892fe6d2c782d6c14fd913a348553928d74d3d
sunny kardam <150102341+Sunny7135@users.noreply.github.com>
2026-09-11 14:59:53 +0530

    Time: 3 ms (26.72%), Space: 52.4 MB (25.89%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int n = nums.size();
+        int step =0;
+        for(int i=0; i<n; i++){
+            if(i>step)return false;
+            step = max(step, nums[i]+i);
+        }
+        return true;
+    }
+};
