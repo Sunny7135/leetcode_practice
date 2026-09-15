491cecb99d9489977d47be74fe273fc003e643d9
sunny kardam <150102341+Sunny7135@users.noreply.github.com>
2026-09-15 16:40:24 +0530

    Time: 0 ms (100%), Space: 20.5 MB (96.22%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
     int jump(vector<int>& nums) {
-        int mx = 0, mn=0, cnt=0;
+        int step=0, mn=0, cnt =0;
         for(int i=0; i<nums.size()-1; i++){
-            mx = max(mx, nums[i] +i);
-            
-            if(i == mn){
+            step = max(step, nums[i]+i);
+
+            if(mn == i){
                 cnt++;
-                mn = mx;
+                mn = step;
             }
         }
         return cnt;
