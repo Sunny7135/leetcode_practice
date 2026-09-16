8b488f382f60095c44a62054d7f8bb568c525599
sunny kardam <150102341+Sunny7135@users.noreply.github.com>
2026-09-16 23:53:49 +0530

    Time: 12 ms (82.93%), Space: 24.9 MB (82.9%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -2,11 +2,11 @@ class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         unordered_map<string, vector<string>>mp;
-        
+
         for(string s : strs){
-            string key = s;
-            sort(key.begin(), key.end());
-            mp[key].push_back(s);
+            string s1 = s;
+            sort(s1.begin(), s1.end());
+            mp[s1].push_back(s);
         }
         vector<vector<string>>ans;
         for(auto &it : mp){
